5bebd90fc6242d95b8ff57cb4afc5f3fbbfe694f
c-chemwa <calebchemwa@gmail.com>
2023-05-29 09:34:51 +0300

    Using a form

--- Landingdata.php
+++ Landingdata.php
@@ -24,5 +24,16 @@
         <p>We will only require a bit of your time!</p>
         <button type="button"><span></span>GET YOUR MEDICINE!</button>
     </div>
+
+    <form action="Landing.php" method="post">
+        First Name: <input type="text" name="F_Name"><br>
+        Last Name: <input type="text" name="L_Name"><br>
+        Date of Birth: <input type="date" name="L_Name"><br>
+        Identification Card Number: <input type="text" name="ID_No"><br>
+        Phone Number: <input type="text" name="Phone_No"><br>
+        <input type="submit">
+    </form>
 </body>
+
 </html>
+
