b16abf4c353618cad64ac813592b0cbfcf13ad2d
Victor Ndung'u <131138431+victorrndungu@users.noreply.github.com>
2023-07-03 12:10:33 +0300

    Update insertPatients.php

--- insertPatients.php
+++ insertPatients.php
@@ -1,13 +1,29 @@
 <?php
-require("EasyDawa.php");
-$sql = "INSERT INTO `patients_info`(`FIRST NAME`, `LAST NAME`, `PATIENT_ID`, `AGE`, `EMAIL ADDRESS`, `GENDER`, `PASSWORDS`) 
-VALUES ('Mindy', 'Milano', '10101020','12', 
-'harry.milano@gmail.com',
-'Female ', 'HarryLano')";
-if ($conn->query($sql) ==TRUE) {
-    echo " New record created successfully";
-} else {
-    echo "Error: " .$sql . "<br>" . $conn->error;
+require("EasyDawa.php"); // Replace with your database connection file
+
+// Function to sanitize input data to prevent SQL injection
+function sanitize($data) {
+    global $conn;
+    return mysqli_real_escape_string($conn, htmlspecialchars(trim($data)));
 }
-$conn->close();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $FIRST_NAME = sanitize($_POST["FIRST_NAME"]);
+    $LAST_NAME = sanitize($_POST["LAST_NAME"]);
+    $GENDER = sanitize($_POST["GENDER"]);
+    $AGE = sanitize($_POST["AGE"]);
+    $EMAIL_ADDRESS = sanitize($_POST["EMAIL_ADDRESS"]);
+
+    // Create the SQL query to insert the new patient into the database
+    $sql = "INSERT INTO patients_info (FIRST_NAME, LAST_NAME, GENDER, AGE, EMAIL_ADDRESS) 
+            VALUES ('$FIRST_NAME', '$LAST_NAME', '$GENDER', '$AGE', '$EMAIL_ADDRESS')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Redirect back to the addPatient.html page with a success message
+        header("Location: addPatient.html?success=true");
+        exit();
+    } else {
+        echo "Error adding new patient: " . $conn->error;
+    }
+}
 ?>
