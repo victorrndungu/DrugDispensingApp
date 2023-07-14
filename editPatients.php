757f7c51826b36af8527cddb46100c182a1c00c1
Victor Ndung'u <131138431+victorrndungu@users.noreply.github.com>
2023-07-14 22:43:08 +0300

    Edit Functionality now functional

--- editPatients.php
+++ editPatients.php
@@ -1,37 +1,49 @@
 <?php
 require("EasyDawa.php");
 
-// Check if the form is submitted
+$PATIENT_ID = $_POST["PATIENT_ID"] ?? null;
+$row = null;
+
+if ($PATIENT_ID) {
+    $sql = "SELECT * FROM patients_info WHERE PATIENT_ID = '$PATIENT_ID'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve and sanitize the form data
-    $PATIENT_ID = $_POST["PATIENT_ID"];
-    $FIRST_NAME = $_POST["FIRST_NAME"];
-    $LAST_NAME = $_POST["LAST_NAME"];
-    $GENDER = $_POST["GENDER"];
-    $AGE = $_POST["AGE"];
-    $EMAIL_ADDRESS = $_POST["EMAIL_ADDRESS"];
-    // Retrieve other necessary form data
+    $FIRST_NAME = $_POST["FIRST_NAME"] ?? "";
+    $LAST_NAME = $_POST["LAST_NAME"] ?? "";
+    $GENDER = $_POST["GENDER"] ?? "";
+    $AGE = $_POST["AGE"] ?? "";
+    $EMAIL_ADDRESS = $_POST["EMAIL_ADDRESS"] ?? "";
 
-    // Update the patient details in the database using an SQL UPDATE query
-
-    // Redirect back to the original page after successful update
-    header("Location: displayPatients.php");
-    exit();
-} else {
-    // Display the form with pre-filled patient details for editing
-    if (isset($_GET["PATIENT_ID"])) {
-        $patientID = $_GET["PATIENT_ID"];
-        // Fetch the patient details from the database based on the ID
-        // Pre-fill the form fields with the fetched details
-        $FIRST_NAME = ""; // Replace with the fetched first name
-        $LAST_NAME = ""; // Replace with the fetched last name
-        $GENDER = ""; // Replace with the fetched gender
-        $AGE = ""; // Replace with the fetched age
-        $EMAIL_ADDRESS = ""; // Replace with the fetched email address
+    // Update the patient details in the database
+    $updateSql = "UPDATE patients_info SET FIRST_NAME = '$FIRST_NAME', LAST_NAME = '$LAST_NAME', GENDER = '$GENDER', AGE = '$AGE', EMAIL_ADDRESS = '$EMAIL_ADDRESS' WHERE PATIENT_ID = '$PATIENT_ID'";
+    if ($conn->query($updateSql) === TRUE) {
+        echo "Patient details updated successfully.";
     } else {
-        echo "Invalid patient ID.";
+        echo "Error updating patient details: " . $conn->error;
     }
 }
 
 $conn->close();
 ?>
+
+<form method="post" action="">
+    <?php if ($PATIENT_ID && isset($row)) : ?>
+        <input type="hidden" name="PATIENT_ID" value="<?php echo $row["PATIENT_ID"]; ?>">
+        <label for="FIRST_NAME">First Name:</label>
+        <input type="text" name="FIRST_NAME" value="<?php echo $row["FIRST_NAME"] ?? ""; ?>"><br>
+        <label for="LAST_NAME">Last Name:</label>
+        <input type="text" name="LAST_NAME" value="<?php echo $row["LAST_NAME"] ?? ""; ?>"><br>
+        <label for="GENDER">Gender:</label>
+        <input type="text" name="GENDER" value="<?php echo $row["GENDER"] ?? ""; ?>"><br>
+        <label for="AGE">Age:</label>
+        <input type="text" name="AGE" value="<?php echo $row["AGE"] ?? ""; ?>"><br>
+        <label for="EMAIL_ADDRESS">Email Address:</label>
+        <input type="text" name="EMAIL_ADDRESS" value="<?php echo $row["EMAIL_ADDRESS"] ?? ""; ?>"><br>
+
+        <input type="submit" value="Update">
+    <?php endif; ?>
+</form>
+
