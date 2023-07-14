80458a2a90fa21cce18bf3f4c28bcaa7a0dec51e
Victor Ndung'u <131138431+victorrndungu@users.noreply.github.com>
2023-07-14 13:05:45 +0300

    Patients Display table
    Edit and Delete functions also present

---
+++ displayPatients.php
@@ -0,0 +1,48 @@
+<?php
+require("EasyDawa.php");
+
+$sql = "SELECT PATIENT_ID, FIRST_NAME, LAST_NAME, GENDER, AGE, EMAIL_ADDRESS FROM patients_info"; 
+$result = $conn->query($sql);
+?>
+
+<?php if ($result->num_rows > 0) : ?>
+    <table border="1">
+        <tr>
+            <th>PATIENT_ID</th>
+            <th>FIRST_NAME</th>
+            <th>LAST_NAME</th>
+            <th>GENDER</th>
+            <th>AGE</th>
+            <th>EMAIL_ADDRESS</th>
+            <th>Actions</th>
+        </tr>
+
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <tr>
+                <td><?php echo $row["PATIENT_ID"]; ?></td>
+                <td><?php echo $row["FIRST_NAME"]; ?></td>
+                <td><?php echo $row["LAST_NAME"]; ?></td>
+                <td><?php echo $row["GENDER"]; ?></td>
+                <td><?php echo $row["AGE"]; ?></td>
+                <td><?php echo $row["EMAIL_ADDRESS"]; ?></td>
+                <td>
+                    <form method="post" action="editPatients.php">
+                        <input type="hidden" name="PATIENT_ID" value="<?php echo $row["PATIENT_ID"]; ?>">
+                        <input type="submit" value="Edit">
+                    </form>
+                    <form method="post" action="deletePatients.php">
+                        <input type="hidden" name="PATIENT_ID" value="<?php echo $row["PATIENT_ID"]; ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+
+    </table>
+<?php else : ?>
+    <p>No data found.</p>
+<?php endif; ?>
+
+<?php
+$conn->close();
+?>
