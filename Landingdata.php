99cdd28078d2820bf3a4b9ff5d29282351cee71e
c-chemwa <calebchemwa@gmail.com>
2023-07-17 15:28:02 +0300

    Added CSS files

--- Landingdata.php
+++ Landingdata.php
@@ -10,11 +10,11 @@
 <body>
     <div class="navigation">
         <nav>
-            <a href="Landingdata.php"><img src="/LandingAssets/White Logo.png" class="logo"></a>
+            <a href="Landingdata.php"><img src="LandingAssets/White Logo.png" class="logo"></a>
             <ul>
                 <li><a href="">Contact</a></li>
-                <li><a href="">Sign Up</a></li>
-                <li><a href="">Log In</a></li>
+                <li><a href="signup.html">Sign Up</a></li>
+                <li><a href="login.html">Log In</a></li>
             </ul>
         </nav>
     </div>
@@ -22,7 +22,7 @@
         <img src="/LandingPage/LandingAssets/White Icon.png" alt="">
         <h1>Get Your Prescriptions today!</h1>
         <p>We will only require a bit of your time!</p>
-        <button type="button"><span></span>GET YOUR MEDICINE!</button>
+        <a href="signup.html"><button type="button"><span></span>GET YOUR MEDICINE!</button></a>
     </div>
 </body>
 
